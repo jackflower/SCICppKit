46e998b5ab1c4a2c6bf10180acb9dc7cd79a19aa
jackflower <jackflower@poczta.onet.pl>
2020-01-14 16:18:46 +0100

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# SCI 2020
+
+Repozytorium - grupa tworząca projekt – grę w języku C/C++ z wykorzystaniem biblioteki SFML
+
+MicorosoftS Visual Studio 2017
+SFML 2.5.0 (SFML-2.5.1-windows-vc15-32-bit)
+
---
+++ SCICppKit/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::String title = L"Szczecińskie Collegium Informatyczne SCI 2020";
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), title);
+
+	
+	sf::Texture ship_graphics;
+	ship_graphics.loadFromFile("../data/sci_ship_one.png");
+	ship_graphics.setSmooth(true);
+
+	sf::Sprite ship;
+	ship.setTexture(ship_graphics);
+	ship.setPosition(400.f, 300.f);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color::White);
+		window.draw(ship);
+		window.display();
+	}
+
+	return 0;
+}
